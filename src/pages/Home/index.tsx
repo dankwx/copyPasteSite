d984f01e1777d3313f410b6585e00ee09d5db4ae
dankwx <danielkondlatsch.p@gmail.com>
2023-11-03 07:04:30 -0300

    feature: estilizacao gif copiado com sucesso

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,8 +8,13 @@ import {
   onSnapshot,
   addDoc,
   DocumentReference,
+  orderBy, // Importe a função orderBy
 } from "firebase/firestore";
+import { Timestamp } from "firebase/firestore";
+
 import styles from "./Home.module.scss";
+import copy from "clipboard-copy";
+import yay from "./piffle-cute.gif";
 
 export default function Home() {
   const app = initializeApp(firebaseConfig);
@@ -17,14 +22,17 @@ export default function Home() {
 
   const [messages, setMessages] = useState<string[]>([]);
   const [text, setText] = useState<string>("");
+  const [copySuccess, setCopySuccess] = useState(false);
 
   const handleSendText = async () => {
     if (text.trim() !== "") {
       try {
+        const now = Timestamp.now();
         const docRef: DocumentReference = await addDoc(
           collection(db, "messages"),
           {
             message: text,
+            timestamp: now,
           }
         );
         console.log("Document written with ID:", docRef.id);
@@ -36,7 +44,7 @@ export default function Home() {
   };
 
   useEffect(() => {
-    const q = query(collection(db, "messages"));
+    const q = query(collection(db, "messages"), orderBy("timestamp", "desc")); // Ordena por "timestamp" em ordem decrescente
 
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       const messageData: string[] = [];
@@ -52,6 +60,17 @@ export default function Home() {
     };
   }, [db]);
 
+  const handleCopyText = (message: string) => {
+    copy(message)
+      .then(() => {
+        setCopySuccess(true);
+        setTimeout(() => setCopySuccess(false), 2500);
+      })
+      .catch((error) => {
+        console.error("Erro ao copiar texto:", error);
+      });
+  };
+
   return (
     <div className={styles.bodyArea}>
       <h1 className={styles.title}>Copiar e Colar</h1>
@@ -63,6 +82,7 @@ export default function Home() {
         value={text}
         onChange={(e) => setText(e.target.value)}
       ></input>
+      
       <button onClick={handleSendText}>
         <span className="circle1"></span>
         <span className="circle2"></span>
@@ -73,11 +93,21 @@ export default function Home() {
       </button>
       <div className={styles.messageArea}>
         {messages.map((message, index) => (
-          <div className={styles.singleMessage} key={index}>
+          <div
+            className={styles.singleMessage}
+            key={index}
+            onClick={() => handleCopyText(message)}
+          >
             <span>{message}</span>
           </div>
         ))}
       </div>
+      {copySuccess && (
+        <div className={styles.copyMessage}>
+          Texto copiado com sucesso!
+          <img src={yay} alt="" />
+        </div>
+      )}
     </div>
   );
 }
