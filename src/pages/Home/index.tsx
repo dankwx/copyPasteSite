4f528af00b413dce675b2af01ed93c56711ec615
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-02-12 18:30:54 -0300

    feature: change input for text area to maintain text format

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { initializeApp } from "firebase/app";
 import firebaseConfig from "../../firebaseConfig";
-import { QueryDocumentSnapshot, DocumentData, doc, getDocs } from 'firebase/firestore';
+import {
+  QueryDocumentSnapshot,
+  DocumentData,
+  doc,
+  getDocs,
+} from "firebase/firestore";
 import {
   getFirestore,
   collection,
@@ -11,7 +16,7 @@ import {
   DocumentReference,
   orderBy,
   deleteDoc,
-  QuerySnapshot
+  QuerySnapshot,
 } from "firebase/firestore";
 import { Timestamp } from "firebase/firestore";
 import Login from "../Login";
@@ -34,10 +39,11 @@ export default function Home() {
     if (text.trim() !== "") {
       try {
         const now = Timestamp.now();
+        const formattedText = text.replace(/\n/g, "<br>"); // Substituir quebras de linha
         const docRef: DocumentReference = await addDoc(
           collection(db, "messages"),
           {
-            message: text,
+            message: formattedText,
             timestamp: now,
           }
         );
@@ -67,7 +73,9 @@ export default function Home() {
   }, [db]);
 
   const handleCopyText = (message: string) => {
-    copy(message)
+    const formattedMessage = message.replace(/\n/g, "<br>");
+
+    copy(formattedMessage)
       .then(() => {
         setCopySuccess(true);
         setTimeout(() => setCopySuccess(false), 2500);
@@ -77,14 +85,11 @@ export default function Home() {
       });
   };
 
-  // Função para verificar se a string é um link
   const isLink = (str: string): boolean => {
-    // Expressão regular simples para verificar se a string se parece com uma URL
     const pattern = /^(https?:\/\/)?([\w.]+)\.([a-z]{2,6}\.?)(\/[\w.]*)*\/?$/i;
     return pattern.test(str);
   };
 
-  // Componente LinkWrapper para tratar links
   const LinkWrapper: React.FC<{ message: string }> = ({ message }) => {
     const isMessageLink = isLink(message);
 
@@ -100,25 +105,19 @@ export default function Home() {
         </a>
       );
     } else {
-      return <span>{message}</span>;
+      // Utilizando dangerouslySetInnerHTML para renderizar HTML de forma segura
+      return <span dangerouslySetInnerHTML={{ __html: message }} />;
     }
   };
 
   const handleDeleteMessage = async (message: string) => {
     try {
-      // Criar a referência da coleção
       const messagesCollection = collection(db, "messages");
-
-      // Criar a query
       const q = query(messagesCollection, orderBy("timestamp", "desc"));
-
-      // Obter o snapshot da query
       const querySnapshot: QuerySnapshot<DocumentData> = await getDocs(q);
+      const docToDelete: QueryDocumentSnapshot<DocumentData> | undefined =
+        querySnapshot.docs.find((doc) => doc.data().message === message);
 
-      // Usando o tipo QueryDocumentSnapshot<DocumentData>
-      const docToDelete: QueryDocumentSnapshot<DocumentData> | undefined = querySnapshot.docs.find(doc => doc.data().message === message);
-
-      // Excluir o documento se encontrado
       if (docToDelete) {
         await deleteDoc(doc(db, "messages", docToDelete.id));
       }
@@ -133,19 +132,18 @@ export default function Home() {
         <div className={styles.bodyArea}>
           <div className={styles.topArea}>
             <h1 className={styles.title}>Copiar e Colar</h1>
-            <input
+            <textarea
               placeholder="Seu texto <3"
-              type="text"
               name="text"
-              className={styles.input}
+              className={styles.textarea}
               value={text}
               onChange={(e) => setText(e.target.value)}
               onKeyDown={(e) => {
-                if (e.key === "Enter") {
+                if (e.key === "Enter" && e.ctrlKey) {
                   handleSendText();
                 }
               }}
-            ></input>
+            ></textarea>
 
             <button onClick={handleSendText}>
               <span className="circle1"></span>
@@ -164,23 +162,20 @@ export default function Home() {
               </div>
             ) : (
               messages.map((message, index) => (
-                <div
-                  className={styles.singleMessage}
-                  key={index}
-                >
+                <div className={styles.singleMessage} key={index}>
                   <div className={styles.upperArea}>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
-                      stroke-width="1.5"
+                      strokeWidth="1.5"
                       stroke="currentColor"
                       className={styles.copyIcon}
                       onClick={() => handleCopyText(message)}
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
                       />
                     </svg>
@@ -188,7 +183,7 @@ export default function Home() {
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
-                      stroke-width="1.5"
+                      strokeWidth="1.5"
                       stroke="currentColor"
                       className={styles.deleteIcon}
                       onClick={(e) => {
@@ -197,8 +192,8 @@ export default function Home() {
                       }}
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
                       />
                     </svg>
