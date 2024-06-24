f01efa0c4312a4835bf49e6e70762163e32a0083
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-06-24 18:14:56 -0300

    feature: add show message filter

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -38,6 +38,7 @@ export default function Home() {
   const [copySuccess, setCopySuccess] = useState(false);
   const isAuthenticated = localStorage.getItem("authenticated");
   const [category, setCategory] = useState<string>("Links"); // Estado inicial, pode ser ajustado conforme necessário
+  const [filterCategory, setFilterCategory] = useState<string>("Todos"); // Estado para controlar a categoria de filtro
 
   const handleSendText = async () => {
     if (text.trim() !== "") {
@@ -64,7 +65,7 @@ export default function Home() {
     const q = query(collection(db, "messages"), orderBy("timestamp", "desc"));
 
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      const messageData: Message[] = []; // Alteração aqui para usar o tipo Message
+      const messageData: Message[] = [];
       querySnapshot.forEach((doc) => {
         const message = doc.data().message;
         const category = doc.data().category;
@@ -84,7 +85,6 @@ export default function Home() {
   }, [db]);
 
   const handleCopyText = (message: Message) => {
-    // Alteração aqui para usar o tipo Message
     const formattedMessage = message.message.replace(/\n/g, "<br>");
 
     copy(formattedMessage)
@@ -103,7 +103,6 @@ export default function Home() {
   };
 
   const LinkWrapper: React.FC<{ message: Message }> = ({ message }) => {
-    // Alteração aqui para usar o tipo Message
     const isMessageLink = isLink(message.message);
 
     if (isMessageLink) {
@@ -123,7 +122,6 @@ export default function Home() {
   };
 
   const handleDeleteMessage = async (message: Message) => {
-    // Alteração aqui para usar o tipo Message
     try {
       const messagesCollection = collection(db, "messages");
       const q = query(messagesCollection, orderBy("timestamp", "desc"));
@@ -175,6 +173,17 @@ export default function Home() {
             <span className="circle5"></span>
             <span className="text">Enviar</span>
           </button>
+          <label htmlFor="Filtro">Filtrar</label>
+          <select
+            name="showOnly"
+            id="showOnly"
+            value={filterCategory}
+            onChange={(e) => setFilterCategory(e.target.value)}
+          >
+            <option value="Todos">Todos</option>
+            <option value="Links">Links</option>
+            <option value="Paleta de Cor">Paleta de Cor</option>
+          </select>
         </div>
         <div className={styles.messageArea}>
           {messages.length === 0 ? (
@@ -182,51 +191,55 @@ export default function Home() {
               <h1>Nenhum texto ainda :(</h1>
             </div>
           ) : (
-            messages.map((message, index) => (
-              <div className={styles.singleMessage} key={index}>
-                <div className={styles.upperArea}>
-                  {/* Aqui você exibe apenas a categoria */}
-                  {message.category}
-
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className={styles.copyIcon}
-                    onClick={() => handleCopyText(message)}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
-                    />
-                  </svg>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className={styles.deleteIcon}
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      handleDeleteMessage(message);
-                    }}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                    />
-                  </svg>
+            messages
+              .filter(
+                (message) =>
+                  filterCategory === "Todos" ||
+                  message.category === filterCategory
+              )
+              .map((message, index) => (
+                <div className={styles.singleMessage} key={index}>
+                  <div className={styles.upperArea}>
+                    {message.category}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className={styles.copyIcon}
+                      onClick={() => handleCopyText(message)}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
+                      />
+                    </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className={styles.deleteIcon}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleDeleteMessage(message);
+                      }}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                      />
+                    </svg>
+                  </div>
+                  <div className={styles.contentArea}>
+                    <LinkWrapper message={message} />
+                  </div>
                 </div>
-                <div className={styles.contentArea}>
-                  <LinkWrapper message={message} />
-                </div>
-              </div>
-            ))
+              ))
           )}
         </div>
         {copySuccess && (
